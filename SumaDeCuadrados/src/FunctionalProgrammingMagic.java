c72978428acf6f9b7f1427877f7c684f4e059a18
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2023-08-17 22:45:39 -0500

    SumaDeCuadrados

---
+++ SumaDeCuadrados/src/FunctionalProgrammingMagic.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class FunctionalProgrammingMagic {
+
+    public static long sumOfSquares(List<Integer> numbers) {
+        
+        // Check if the list is null. If so, return 0.
+        if (numbers == null){
+            return 0;
+        }
+        // Stream over the list of numbers, find squares and sum them up
+        return numbers.stream().mapToLong(d -> d * d).sum();
+    }
+
+}
